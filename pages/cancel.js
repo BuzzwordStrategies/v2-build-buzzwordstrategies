eaa6be6e0decaed7713fd8549943876c5a6acb0d
BuzzwordStrategies <josh@buzzwordstrategies.com>
2025-05-23 14:52:45 -0600

    Update cancel.js

--- pages/cancel.js
+++ pages/cancel.js
@@ -1,1 +1,9 @@
-
+export default function Cancel() {
+  return (
+    <div>
+      <h1>Payment Cancelled</h1>
+      <p>Your order was not completed. You can try again.</p>
+      <a href="/">Go back to start</a>
+    </div>
+  );
+}
