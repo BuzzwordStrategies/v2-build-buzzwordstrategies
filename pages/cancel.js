83ccf84db84c290091e4dff3d280dd590e043688
BuzzwordStrategies <josh@buzzwordstrategies.com>
2025-05-23 15:18:33 -0600

    Delete pages directory

--- pages/cancel.js
+++
@@ -1,9 +0,0 @@
-export default function Cancel() {
-  return (
-    <div>
-      <h1>Payment Cancelled</h1>
-      <p>Your order was not completed. You can try again.</p>
-      <a href="/">Go back to start</a>
-    </div>
-  );
-}
